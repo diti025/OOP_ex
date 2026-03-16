7ef61425b7c3175f1fa73c63f0b70ed99c5fbb20
diti025 <oatcake.cadets_6g@icloud.com>
2026-03-16 19:04:30 +0200

    Add files via upload

---
+++ src/main/java/main/RaceResult.java
@@ -0,0 +1,11 @@
+package main;
+import java.util.Map;
+
+public interface RaceResult {
+
+    void recordResult(Driver driver, int position);
+
+    Map<Driver, Integer> getResults();
+
+    String getRaceName();
+}
